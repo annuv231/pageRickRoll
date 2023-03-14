0ab6037104eb3b2006cb764c083fcc2c647493c0
Amlendra verma <33187053+annuv231@users.noreply.github.com>
2023-03-14 16:16:30 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,13 @@
 const image = "https://media.tenor.com/PehuVp_jAjUAAAAC/popular-controversial.gif";
 
+//run once it loads
+const images = document.getElementsByTagName("img");
+  for (let i = 0; i < images.length; i++) {
+    images[i].src = image;
+    console.log("getting images");
+  }
 setInterval(() => {
+  //will fetch remaining images after loading.
   const images = document.getElementsByTagName("img");
   for (let i = 0; i < images.length; i++) {
     images[i].src = image;
